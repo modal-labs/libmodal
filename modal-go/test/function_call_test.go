6da14af481dbeec719c46a69ddcd01cacf342883
Luis Capelo <luis@modal.com>
2025-05-07 09:42:23 -0400

    Adds test for cancel

--- modal-go/test/function_call_test.go
+++ modal-go/test/function_call_test.go
@@ -35,4 +35,23 @@ func TestFunctionSpawn(t *testing.T) {
 	g.Expect(err).ShouldNot(gomega.HaveOccurred())
 	g.Expect(result).Should(gomega.Equal("output: hello"))
 
+	// Get function that
+	functionSleep, err := modal.FunctionLookup(
+		context.Background(),
+		"libmodal-test-support", "sleep", modal.LookupOptions{},
+	)
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	functionCall, err = functionSleep.Spawn(nil, map[string]any{"t": 5})
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	// Cancel function call
+	terminateContainers := false // leave test containers running
+	err = functionCall.Cancel(terminateContainers)
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	// Attempting to get cancelled input fails
+	_, err = functionCall.Get()
+	g.Expect(err).Should(gomega.HaveOccurred())
+
 }
