1e90f552ec9e749438144c84a41d8b32fe140fe5
Thomas J. Fan <thomas@modal.com>
2025-06-23 19:13:02 +0000

    Add objects to index

--- modal-js/src/index.ts
+++ modal-js/src/index.ts
@@ -34,3 +34,4 @@ export type { ExecOptions, StdioBehavior, StreamMode } from "./sandbox";
 export { ContainerProcess, Sandbox } from "./sandbox";
 export type { ModalReadStream, ModalWriteStream } from "./streams";
 export { Secret, type SecretFromNameOptions } from "./secret";
+export { FileHandle, FileMode, ReadOptions, WriteOptions } from "./file_handle";
