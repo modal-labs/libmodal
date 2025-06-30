3766c282540bc7f70e7807bb6d7e521c995b235b
Thomas J. Fan <thomas@modal.com>
2025-06-30 19:55:03 +0000

    Lint

--- modal-js/examples/init-client.ts
+++ modal-js/examples/init-client.ts
@@ -4,14 +4,14 @@ import { Function_, initializeClient } from "modal";
 
 const modalId = process.env.CUSTOM_MODAL_ID;
 if (!modalId) {
-    throw new Error("CUSTOM_MODAL_ID environment variable not set");
+  throw new Error("CUSTOM_MODAL_ID environment variable not set");
 }
 const modalSecret = process.env.CUSTOM_MODAL_SECRET;
 if (!modalSecret) {
-    throw new Error("CUSTOM_MODAL_SECRET environment variable not set");
+  throw new Error("CUSTOM_MODAL_SECRET environment variable not set");
 }
 
-initializeClient({tokenId: modalId, tokenSecret: modalSecret})
+initializeClient({ tokenId: modalId, tokenSecret: modalSecret });
 
 const echo = await Function_.lookup("libmodal-test-support", "echo_string");
-console.log(echo)
+console.log(echo);
