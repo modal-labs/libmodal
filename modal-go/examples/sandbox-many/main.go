6c007d9426c4c47e27b1ad8f143f772721934414
Thomas J. Fan <thomas@modal.com>
2025-08-21 14:17:38 +0000

    Quick load test

---
+++ modal-go/examples/sandbox-many/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/modal-labs/libmodal/modal-go"
+)
+
+func main() {
+	ctx := context.Background()
+
+	app, err := modal.AppLookup(ctx, "libmodal-example", &modal.LookupOptions{CreateIfMissing: true})
+	if err != nil {
+		log.Fatalf("Failed to lookup or create app: %v", err)
+	}
+
+	image := modal.NewImageFromRegistry("alpine:3.21", nil)
+
+	type SandboxWithTunnel struct {
+		sandbox *modal.Sandbox
+		tunnels map[int]*modal.Tunnel
+	}
+
+	sandboxes := []SandboxWithTunnel{}
+
+	for i := 0; i < 20; i++ {
+		sb, err := app.CreateSandbox(image, nil)
+		if err != nil {
+			log.Fatalf("Failed to create sandbox: %v", err)
+		}
+		sb.Exec([]string{"echo", fmt.Sprintf("%d", i)}, modal.ExecOptions{})
+		tunnels, err := sb.Tunnels(30 * time.Second)
+		if err != nil {
+			log.Fatalf("Failed to create sandbox: %v", err)
+		}
+
+		sandboxes = append(sandboxes, SandboxWithTunnel{sandbox: sb, tunnels: tunnels})
+	}
+
+	for _, sandbox := range sandboxes {
+		sandbox.sandbox.Terminate()
+	}
+
+}
