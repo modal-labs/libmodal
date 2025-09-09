57426621c26f2d76fd01680678a19cd7a14ef65c
Eric Hansander <eric.hansander@modal.com>
2025-09-09 20:35:52 +0200

    Image.DockerfileCommands for Go

--- modal-go/test/image_test.go
+++ modal-go/test/image_test.go
@@ -2,9 +2,12 @@ package test
 
 import (
 	"context"
+	"io"
 	"testing"
 
 	"github.com/modal-labs/libmodal/modal-go"
+	pb "github.com/modal-labs/libmodal/modal-go/proto/modal_proto"
+	"github.com/modal-labs/libmodal/modal-go/testsupport/grpcmock"
 	"github.com/onsi/gomega"
 )
 
@@ -214,4 +217,223 @@ func TestImageDelete(t *testing.T) {
 
 	_, err = modal.NewImageFromId(ctx, "im-nonexistent")
 	g.Expect(err).Should(gomega.MatchError(gomega.MatchRegexp("Image .+ not found")))
+}
+
+func TestDockerfileCommands(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewWithT(t)
+
+	app, err := modal.AppLookup(context.Background(), "libmodal-test", &modal.LookupOptions{CreateIfMissing: true})
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	image := modal.NewImageFromRegistry("alpine:3.21", nil).DockerfileCommands(
+		[]string{"RUN echo hey > /root/hello.txt"},
+		nil,
+	)
+
+	sb, err := app.CreateSandbox(image, &modal.SandboxOptions{
+		Command: []string{"cat", "/root/hello.txt"},
+	})
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	stdout, err := io.ReadAll(sb.Stdout)
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+	g.Expect(string(stdout)).Should(gomega.Equal("hey\n"))
+
+	err = sb.Terminate()
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+}
+
+func TestDockerfileCommandsEmptyArrayNoOp(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewWithT(t)
+
+	image1 := modal.NewImageFromRegistry("alpine:3.21", nil)
+	image2 := image1.DockerfileCommands([]string{}, nil)
+	g.Expect(image2).Should(gomega.BeIdenticalTo(image1))
+}
+
+func TestDockerfileCommandsChaining(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewWithT(t)
+
+	app, err := modal.AppLookup(context.Background(), "libmodal-test", &modal.LookupOptions{CreateIfMissing: true})
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	secret, err := modal.SecretFromMap(context.Background(), map[string]string{"SECRET": "hello"}, nil)
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	image := modal.NewImageFromRegistry("alpine:3.21", nil).
+		DockerfileCommands([]string{"RUN echo ${SECRET:-unset} > /root/layer1.txt"}, nil).
+		DockerfileCommands([]string{"RUN echo ${SECRET:-unset} > /root/layer2.txt"}, &modal.ImageDockerfileCommandsOptions{
+			Secrets: []*modal.Secret{secret},
+		}).
+		DockerfileCommands([]string{"RUN echo ${SECRET:-unset} > /root/layer3.txt"}, nil)
+
+	sb, err := app.CreateSandbox(image, &modal.SandboxOptions{
+		Command: []string{
+			"cat",
+			"/root/layer1.txt",
+			"/root/layer2.txt",
+			"/root/layer3.txt",
+		},
+	})
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	stdout, err := io.ReadAll(sb.Stdout)
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+	g.Expect(string(stdout)).Should(gomega.Equal("unset\nhello\nunset\n"))
+
+	err = sb.Terminate()
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+}
+
+func TestDockerfileCommandsCopyCommandValidation(t *testing.T) {
+	t.Parallel()
+	g := gomega.NewWithT(t)
+
+	app, err := modal.AppLookup(context.Background(), "libmodal-test", &modal.LookupOptions{CreateIfMissing: true})
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	image := modal.NewImageFromRegistry("alpine:3.21", nil)
+
+	validImage := image.DockerfileCommands(
+		[]string{"COPY --from=alpine:latest /etc/os-release /tmp/os-release"},
+		nil,
+	)
+	_, err = validImage.Build(app)
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	invalidImage := image.DockerfileCommands(
+		[]string{"COPY ./file.txt /root/"},
+		nil,
+	)
+	_, err = invalidImage.Build(app)
+	g.Expect(err).Should(gomega.HaveOccurred())
+	g.Expect(err.Error()).Should(gomega.ContainSubstring("COPY commands that copy from local context are not yet supported"))
+
+	runImage := image.DockerfileCommands(
+		[]string{"RUN echo 'COPY ./file.txt /root/'"},
+		nil,
+	)
+	_, err = runImage.Build(app)
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+
+	multiInvalidImage := image.DockerfileCommands(
+		[]string{
+			"RUN echo hey",
+			"copy ./file.txt /root/",
+			"RUN echo hey",
+		},
+		nil,
+	)
+	_, err = multiInvalidImage.Build(app)
+	g.Expect(err).Should(gomega.HaveOccurred())
+	g.Expect(err.Error()).Should(gomega.ContainSubstring("COPY commands that copy from local context are not yet supported"))
+}
+
+func TestDockerfileCommandsWithOptions(t *testing.T) {
+	g := gomega.NewWithT(t)
+
+	mock, cleanup := grpcmock.Install()
+	t.Cleanup(cleanup)
+
+	grpcmock.HandleUnary(
+		mock, "ImageGetOrCreate",
+		func(req *pb.ImageGetOrCreateRequest) (*pb.ImageGetOrCreateResponse, error) {
+			g.Expect(req.GetAppId()).To(gomega.Equal("ap-test"))
+			image := req.GetImage()
+			g.Expect(image.GetDockerfileCommands()).To(gomega.Equal([]string{"FROM alpine:3.21"}))
+			g.Expect(image.GetSecretIds()).To(gomega.BeEmpty())
+			g.Expect(image.GetBaseImages()).To(gomega.BeEmpty())
+			g.Expect(image.GetGpuConfig()).To(gomega.BeNil())
+			g.Expect(req.GetForceBuild()).To(gomega.BeFalse())
+
+			return pb.ImageGetOrCreateResponse_builder{
+				ImageId: "im-base",
+				Result:  pb.GenericResult_builder{Status: pb.GenericResult_GENERIC_STATUS_SUCCESS}.Build(),
+			}.Build(), nil
+		},
+	)
+
+	grpcmock.HandleUnary(
+		mock, "ImageGetOrCreate",
+		func(req *pb.ImageGetOrCreateRequest) (*pb.ImageGetOrCreateResponse, error) {
+			g.Expect(req.GetAppId()).To(gomega.Equal("ap-test"))
+			image := req.GetImage()
+			g.Expect(image.GetDockerfileCommands()).To(gomega.Equal([]string{"FROM base", "RUN echo layer1"}))
+			g.Expect(image.GetSecretIds()).To(gomega.BeEmpty())
+			g.Expect(image.GetBaseImages()).To(gomega.HaveLen(1))
+			g.Expect(image.GetBaseImages()[0].GetDockerTag()).To(gomega.Equal("base"))
+			g.Expect(image.GetBaseImages()[0].GetImageId()).To(gomega.Equal("im-base"))
+			g.Expect(image.GetGpuConfig()).To(gomega.BeNil())
+			g.Expect(req.GetForceBuild()).To(gomega.BeFalse())
+
+			return pb.ImageGetOrCreateResponse_builder{
+				ImageId: "im-layer1",
+				Result:  pb.GenericResult_builder{Status: pb.GenericResult_GENERIC_STATUS_SUCCESS}.Build(),
+			}.Build(), nil
+		},
+	)
+
+	grpcmock.HandleUnary(
+		mock, "ImageGetOrCreate",
+		func(req *pb.ImageGetOrCreateRequest) (*pb.ImageGetOrCreateResponse, error) {
+			g.Expect(req.GetAppId()).To(gomega.Equal("ap-test"))
+			image := req.GetImage()
+			g.Expect(image.GetDockerfileCommands()).To(gomega.Equal([]string{"FROM base", "RUN echo layer2"}))
+			g.Expect(image.GetSecretIds()).To(gomega.Equal([]string{"sc-test"}))
+			g.Expect(image.GetBaseImages()).To(gomega.HaveLen(1))
+			g.Expect(image.GetBaseImages()[0].GetDockerTag()).To(gomega.Equal("base"))
+			g.Expect(image.GetBaseImages()[0].GetImageId()).To(gomega.Equal("im-layer1"))
+			g.Expect(image.GetGpuConfig()).ToNot(gomega.BeNil())
+			g.Expect(image.GetGpuConfig().GetType()).To(gomega.Equal(pb.GPUType(0)))
+			g.Expect(image.GetGpuConfig().GetCount()).To(gomega.Equal(uint32(1)))
+			g.Expect(image.GetGpuConfig().GetGpuType()).To(gomega.Equal("A100"))
+			g.Expect(req.GetForceBuild()).To(gomega.BeTrue())
+
+			return pb.ImageGetOrCreateResponse_builder{
+				ImageId: "im-layer2",
+				Result:  pb.GenericResult_builder{Status: pb.GenericResult_GENERIC_STATUS_SUCCESS}.Build(),
+			}.Build(), nil
+		},
+	)
+
+	grpcmock.HandleUnary(
+		mock, "ImageGetOrCreate",
+		func(req *pb.ImageGetOrCreateRequest) (*pb.ImageGetOrCreateResponse, error) {
+			g.Expect(req.GetAppId()).To(gomega.Equal("ap-test"))
+			image := req.GetImage()
+			g.Expect(image.GetDockerfileCommands()).To(gomega.Equal([]string{"FROM base", "RUN echo layer3"}))
+			g.Expect(image.GetSecretIds()).To(gomega.BeEmpty())
+			g.Expect(image.GetBaseImages()).To(gomega.HaveLen(1))
+			g.Expect(image.GetBaseImages()[0].GetDockerTag()).To(gomega.Equal("base"))
+			g.Expect(image.GetBaseImages()[0].GetImageId()).To(gomega.Equal("im-layer2"))
+			g.Expect(image.GetGpuConfig()).To(gomega.BeNil())
+			g.Expect(req.GetForceBuild()).To(gomega.BeTrue())
+
+			return pb.ImageGetOrCreateResponse_builder{
+				ImageId: "im-layer3",
+				Result:  pb.GenericResult_builder{Status: pb.GenericResult_GENERIC_STATUS_SUCCESS}.Build(),
+			}.Build(), nil
+		},
+	)
+
+	app := &modal.App{AppId: "ap-test"}
+	secret := &modal.Secret{SecretId: "sc-test"}
+
+	builtImage, err := modal.NewImageFromRegistry("alpine:3.21", nil).
+		DockerfileCommands([]string{"RUN echo layer1"}, nil).
+		DockerfileCommands([]string{"RUN echo layer2"}, &modal.ImageDockerfileCommandsOptions{
+			Secrets:    []*modal.Secret{secret},
+			GPU:        "A100",
+			ForceBuild: true,
+		}).
+		DockerfileCommands([]string{"RUN echo layer3"}, &modal.ImageDockerfileCommandsOptions{
+			ForceBuild: true,
+		}).
+		Build(app)
+
+	g.Expect(err).ShouldNot(gomega.HaveOccurred())
+	g.Expect(builtImage.ImageId).To(gomega.Equal("im-layer3"))
 }
