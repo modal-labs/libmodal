62b9559be29e689ef44b6afce200aefeae322109
Eric Hansander <eric.hansander@modal.com>
2025-10-20 12:45:16 +0200

    Fix version format tests

--- modal-js/test/version_tracking.test.ts
+++ modal-js/test/version_tracking.test.ts
@@ -5,13 +5,13 @@ declare const __MODAL_SDK_VERSION__: string;
 
 test("VersionConstantFormat", () => {
   expect(__MODAL_SDK_VERSION__).toBeDefined();
-  expect(__MODAL_SDK_VERSION__).toMatch(/^\d+\.\d+\.\d+$/);
+  expect(__MODAL_SDK_VERSION__).toMatch(/^\d+\.\d+\.\d+(-dev\.\d+)?$/);
 });
 
 test("ClientVersion", () => {
   const client = new ModalClient();
   expect(client.version()).toBeDefined();
-  expect(client.version()).toMatch(/^\d+\.\d+\.\d+$/);
+  expect(client.version()).toMatch(/^\d+\.\d+\.\d+(-dev\.\d+)?$/);
   expect(client.version()).toBe(__MODAL_SDK_VERSION__);
   client.close();
 });
