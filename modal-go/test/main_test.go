d23a6175691954234ef589c4b369f9b62093963e
Eric Hansander <eric.hansander@modal.com>
2025-11-11 11:15:08 +0100

    Enable Client cleanup before leak detection

--- modal-go/test/main_test.go
+++ modal-go/test/main_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"context"
 	"flag"
+	"net/http"
 	"os"
 	"testing"
 
@@ -14,28 +15,42 @@ import (
 // tc is the test Client, used for running tests against Modal infra.
 var tc *modal.Client
 
-// checkLeaks enables package-level goroutine leak detection using goleak.VerifyTestMain.
-// This approach works with t.Parallel() tests.
-var checkLeaks = flag.Bool("check-leaks", false, "enable package-level goroutine leak detection (works with t.Parallel() tests)")
+// testMainWrapper wraps testing.M, so we can do cleanup before goleak looks for leaks.
+// We need to do cleanup (closing the test client etc.) after m.Run() and before
+// goleak.Find(), but goleak.VerifyTestMain doesn't offer a way to do this (goleak.Cleanup()
+// is deferred until after goleak.Find() returns).
+type testMainWrapper struct {
+	m *testing.M
+}
 
-var goleakOptions []goleak.Option
-
-func TestMain(m *testing.M) {
-	flag.Parse()
-
+func (w *testMainWrapper) Run() int {
 	c, err := modal.NewClient()
 	if err != nil {
 		panic(err)
 	}
 	tc = c
+	defer c.Close()
+	defer http.DefaultClient.CloseIdleConnections()
+	return w.m.Run()
+}
 
-	// Capture baseline goroutines after client creation
-	goleakOptions = append(goleakOptions, goleak.IgnoreCurrent())
+// checkLeaks enables package-level goroutine leak detection using goleak.VerifyTestMain.
+// This approach works with t.Parallel() tests.
+var checkLeaks = flag.Bool("check-leaks", false, "enable package-level goroutine leak detection (works with t.Parallel() tests)")
 
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	wrapper := &testMainWrapper{m: m}
 	if *checkLeaks {
-		goleak.VerifyTestMain(m, goleakOptions...)
+		var goleakOptions []goleak.Option
+		// Capture baseline goroutines before any test infrastructure is created.
+		// The wrapper will create/close the client within its Run() call.
+		goleakOptions = append(goleakOptions, goleak.IgnoreCurrent())
+
+		goleak.VerifyTestMain(wrapper, goleakOptions...)
 	} else {
-		code := m.Run()
+		code := wrapper.Run()
 		os.Exit(code)
 	}
 }
