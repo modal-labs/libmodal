ac9e6f069ad5267212daf9a6ba97bdc9de77b13e
Thomas J. Fan <thomas@modal.com>
2026-01-28 20:28:04 -0500

    Do not detach

--- modal-go/test/sandbox_filesystem_snapshot_test.go
+++ modal-go/test/sandbox_filesystem_snapshot_test.go
@@ -36,7 +36,7 @@ func TestSnapshotFilesystem(t *testing.T) {
 	g.Expect(snapshotImage).ShouldNot(gomega.BeNil())
 	g.Expect(snapshotImage.ImageID).To(gomega.HavePrefix("im-"))
 
-	err = sb.Terminate(ctx, true, nil)
+	err = sb.Terminate(ctx, false, nil)
 	g.Expect(err).ShouldNot(gomega.HaveOccurred())
 
 	sb2, err := tc.Sandboxes.Create(ctx, app, snapshotImage, nil)
