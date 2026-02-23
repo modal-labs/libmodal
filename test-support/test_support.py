915e190ddfed5e703fa06447bb7a93e80272b3e4
Thomas J. Fan <thomas@modal.com>
2026-02-23 12:20:06 -0500

    Do not use min containers for test

--- test-support/test_support.py
+++ test-support/test_support.py
@@ -7,39 +7,39 @@ import modal
 app = modal.App("libmodal-test-support")
 
 
-@app.function(min_containers=1, _experimental_restrict_output=True)
+@app.function(timeout=60 * 5, _experimental_restrict_output=True)
 def echo_string(s: str) -> str:
     return "output: " + s
 
 
-@app.function(min_containers=1, _experimental_restrict_output=True)
+@app.function(timeout=60 * 5, _experimental_restrict_output=True)
 def identity_with_repr(s: typing.Any) -> typing.Any:
     return s, repr(s)
 
 
-@app.function(min_containers=1)
+@app.function(timeout=60 * 5)
 def sleep(t: int) -> None:
     time.sleep(t)
 
 
-@app.function(min_containers=1)
+@app.function(timeout=60 * 5)
 def bytelength(buf: bytes) -> int:
     return len(buf)
 
 
-@app.function(min_containers=1, experimental_options={"input_plane_region": "us-west"})
+@app.function(timeout=60 * 5, experimental_options={"input_plane_region": "us-west"})
 def input_plane(s: str) -> str:
     return "output: " + s
 
 
-@app.cls(min_containers=1)
+@app.cls(timeout=60 * 5)
 class EchoCls:
     @modal.method()
     def echo_string(self, s: str) -> str:
         return "output: " + s
 
 
-@app.cls(min_containers=1, experimental_options={"input_plane_region": "us-west"})
+@app.cls(timeout=60 * 5, experimental_options={"input_plane_region": "us-west"})
 class EchoClsInputPlane:
     @modal.method()
     def echo_string(self, s: str) -> str:
