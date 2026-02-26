1e2fe92988f5c354833615903b75536453a69154
Lucy Zhang <36051089+zhang-lucy@users.noreply.github.com>
2026-02-26 10:33:15 -0500

    Fix error message when Sandbox already completed (#288)
    * Fix error message when Sandbox already completed
    
    * Run linter
    
    * Update changelog
    
    ---------
    
    Co-authored-by: Thomas J. Fan <thomas@modal.com>

--- modal-js/src/sandbox.ts
+++ modal-js/src/sandbox.ts
@@ -957,8 +957,15 @@ export class Sandbox {
         sandboxId: this.sandboxId,
       });
       if (resp.taskResult) {
+        if (
+          resp.taskResult.status ===
+            GenericResult_GenericStatus.GENERIC_STATUS_SUCCESS ||
+          !resp.taskResult.exception
+        ) {
+          throw new Error(`Sandbox ${this.sandboxId} has already completed`);
+        }
         throw new Error(
-          `Sandbox ${this.sandboxId} has already completed with result: ${resp.taskResult}`,
+          `Sandbox ${this.sandboxId} has already completed with result: exception:"${resp.taskResult.exception}"`,
         );
       }
       if (resp.taskId) {
